3a20706a8fc7b648f6ddf9fe7b0f70f5717a5d00
Sam King <samuel-king@live.com.au>
2020-05-21 12:56:26 +1000

    Cleaned up backtracking solver

--- solver/BackTrackingSolver.java
+++ solver/BackTrackingSolver.java
@@ -14,25 +14,12 @@ import grid.SudokuGrid;
  * Backtracking solver for standard Sudoku.
  */
 public class BackTrackingSolver extends StdSudokuSolver {
-	StdSudokuGrid grid;
-	ArrayList<Integer> values;
-	int valueIndex;
-	int gridSize;
-	int numCells;
-	boolean complete;
-
-	boolean[] visited;
-	int count;
-
-	int[] lastSuccessfulCell;
-	int cellIndex;
+	
+	private StdSudokuGrid grid;
+	private ArrayList<Integer> values;
+	private int gridSize;
 
 	public BackTrackingSolver() {
-
-		this.valueIndex = 0;
-		this.complete = false;
-		this.cellIndex = 0;
-
 	} // end of BackTrackingSolver()
 
 	@Override
@@ -42,28 +29,10 @@ public class BackTrackingSolver extends StdSudokuSolver {
 		this.grid = (StdSudokuGrid) grid;
 		this.gridSize = this.grid.getGridSize();
 		this.values = this.grid.getValues();
-		this.numCells = this.gridSize * this.gridSize;
 
-		// Holds last successfull co-ordinate, and the index of the value placed there.
-		this.lastSuccessfulCell = new int[] { 0, 0, 0 };
-		// this.solveNext(0,0,0);
+		boolean result = this.recursiveSolver();
 
-		// TODO we need to enumerate the co-ordinates of every blank cell to visit, then
-		// go through those co-ordinates one by one
-		// Instead of only changing blank co-ordinates in solveNext().
-
-		/*
-		 * global var lastSuccessfulCellIndex[][]
-		 * 
-		 * sudokuSolver(cell) { if (isCellValid(cell)) { lasSuccessfulCellIndex = cell
-		 * foreach (cell c : cellsRemaining) { sudokuSolver(c); } } else {
-		 * 
-		 * sudokuSolver(lastSuccessfulCellIndex); }
-		 */
-
-		this.recursiveSolver();
-
-		return true;
+		return result;
 	} // end of solve()
 
 	public boolean recursiveSolver() {
