e39a021561f3e9d4e890ae574014d0a64181110e
Sam King <samuel-king@live.com.au>
2020-05-27 23:23:27 +1000

    Binary matrix complete, misc refactoring

--- solver/AlgorXSolver.java
+++ solver/AlgorXSolver.java
@@ -8,72 +8,114 @@ import java.util.ArrayList;
 import grid.StdSudokuGrid;
 import grid.SudokuGrid;
 
-
 /**
  * Algorithm X solver for standard Sudoku.
  */
-public class AlgorXSolver extends StdSudokuSolver
-{
-	
+public class AlgorXSolver extends StdSudokuSolver {
+
 	private StdSudokuGrid grid;
 	private ArrayList<Integer> values;
 	private int gridSize;
-	
+	private int matrixRows;
+	private int matrixCols;
+	private int numCells;
+
 	private ArrayList<ArrayList<String>> binaryMatrix;
 
-    public AlgorXSolver() {
-       
-    } // end of AlgorXSolver()
+	public AlgorXSolver() {
 
+	} // end of AlgorXSolver()
 
-    @Override
-    public boolean solve(SudokuGrid grid) {
-        
-    	this.grid = (StdSudokuGrid) grid;
+	@Override
+	public boolean solve(SudokuGrid grid) {
+
+		this.grid = (StdSudokuGrid) grid;
 		this.gridSize = this.grid.getGridSize();
 		this.values = this.grid.getValues();
-		
-		int matrixRows = (int) Math.pow(this.gridSize, 3);
-		int matrixCols = (int) Math.pow(this.gridSize, 2) * 4;
-		
-		//Initialize and populate the exact cover binary matrix. 
+		this.numCells = gridSize * gridSize;
+
+		this.matrixRows = (int) Math.pow(this.gridSize, 3);
+		this.matrixCols = (int) Math.pow(this.gridSize, 2) * 4;
+
+		// Initialize and populate the exact cover binary matrix with 0's.
 		this.binaryMatrix = new ArrayList<ArrayList<String>>();
-		
+
 		for (int i = 0; i < matrixRows; i++) {
 			binaryMatrix.add(new ArrayList<String>());
 		}
-		
+
 		for (int i = 0; i < matrixRows; i++) {
 			for (int j = 0; j < matrixCols; j++) {
 				binaryMatrix.get(i).add("0");
 			}
 		}
+
+		// Add cell constrains to binary matrix.
+		int matrixStartingIndex = 0;
+		this.cellConstraints(matrixStartingIndex);
+
+		// Add row constrains to binary matrix.
+		matrixStartingIndex += numCells;
+		this.rowConstraints(matrixStartingIndex);
 		
-		//Cell constraints
-		int index = 0;
+		// Add column constraints to binary matrix.
+		matrixStartingIndex += numCells;
+		columnConstraints(matrixStartingIndex);
+
+		// Add box constraints to binary matrix
+		matrixStartingIndex += numCells;
+		int boxWidth = (int) Math.sqrt(gridSize);
+		this.boxConstraints(0, 0, boxWidth, 0, matrixStartingIndex);
+
+		// Print Matrix for testing
+		StringBuilder builder = new StringBuilder("");
+		for (int i = 0; i < matrixRows; i++) {
+			for (int j = 0; j < matrixCols; j++) {
+				builder.append(binaryMatrix.get(i).get(j) + " ");
+			}
+			builder.append("\n");
+		}
+		System.out.println(builder);
+
+		// placeholder
+		return false;
+	} // end of solve()
+
+	/*
+	 * Fills matrix with appropriate column constraints
+	 */
+	public void columnConstraints(int index) {
+
+		index = gridSize * gridSize * 2;
 		int count = 0;
 		for (int i = 0; i < matrixRows; i++) {
-			for (int j = 0; j < gridSize * gridSize; j++) {
+			for (int j = index; j < gridSize * gridSize * 3; j++) {
 				if (j == index) {
-					binaryMatrix.get(i).set(j, "1");;
+					binaryMatrix.get(i).set(j, "1");
+					;
+					index++;
 					count++;
-					if (count == gridSize) {
-						index++;
-						count = 0;
-						break;
-					}
+					break;
 				}
 			}
+			if (count == gridSize * gridSize) {
+				index = gridSize * gridSize * 2;
+				count = 0;
+			}
 		}
-		
-		//Row Constraints
-		index = gridSize * gridSize;
-		count = 0;
+	}
+	/*
+	 * Fills matrix with appropriate row constraints
+	 */
+	public void rowConstraints(int index) {
+
+		int count = 0;
 		int iteration = 0;
 		for (int i = 0; i < matrixRows; i++) {
 			for (int j = index; j < index * 2; j++) {
 				if (j == index) {
-					binaryMatrix.get(i).set(j, "1");;
+					binaryMatrix.get(i).set(j, "1");
+					;
 					index++;
 					count++;
 					if (count == gridSize) {
@@ -89,80 +131,51 @@ public class AlgorXSolver extends StdSudokuSolver
 				iteration = 0;
 			}
 		}
-		
-		//Column Constraints
-		index = gridSize * gridSize * 2;
-		count = 0;
+	}
+	/*
+	 * Fills matrix with appropriate cell constraints
+	 */
+	public void cellConstraints(int matrixStartingIndex) {
+
+		int count = 0;
 		for (int i = 0; i < matrixRows; i++) {
-			for (int j = index; j < gridSize * gridSize * 3; j++) {
-				if (j == index) {
-					binaryMatrix.get(i).set(j, "1");;
-					index++;
+			for (int j = 0; j < gridSize * gridSize; j++) {
+				if (j == matrixStartingIndex) {
+					binaryMatrix.get(i).set(j, "1");
+					;
 					count++;
-					break;
+					if (count == gridSize) {
+						matrixStartingIndex++;
+						count = 0;
+						break;
+					}
 				}
 			}
-			if (count == gridSize * gridSize) {
-				index = gridSize * gridSize * 2;
-				count = 0;
-			}
 		}
-		
-		//Box Constraints
-		int boxWidth = (int) Math.sqrt(gridSize);
-		this.boxConstraints(0,0,boxWidth,0);
-		
-		
-		
-		
-		StringBuilder builder = new StringBuilder("");
-		int h = 0;
-		for (int i = 0; i < matrixRows; i++) {
-			for (int j = 0; j < matrixCols; j++) {
-				builder.append(binaryMatrix.get(i).get(j) + " ");
-
-			}
-			builder.append("\n");
-			h++;
-			if (h == 4) {
-				//returnString += "\n";
-				h = 0;
-			}
-		}
-		System.out.println(builder);
-		
-		
-		
-        // placeholder
-        return false;
-    } // end of solve()
-    
-    
-    public void boxConstraints(int coord1, int coord2, int boxWidth, int boxNum) {
+	}
+	/*
+	 * Recursively fills matrix with appropriate box constraints.
+	 */
+	public void boxConstraints(int coord1, int coord2, int boxWidth, int boxNum, int matrixStartIndex) {
 
 		int numBoxes = boxWidth * boxWidth;
-		int matrixStartIndex = numBoxes * numBoxes * 3;
 		int boxStartingIndex = boxNum * numBoxes;
-		
+
 		// Stopping condition
 		if (coord1 == numBoxes) {
 			// terminate quietly.
 		} else {
-			//System.out.println(coord1 + " " + coord2);
-			// Get all box values.
-			//System.out.println(boxStartingIndex);
+			// Cycle through all box cells.
 			for (int i = coord1; i < coord1 + boxWidth; i++) {
 				for (int j = coord2; j < coord2 + boxWidth; j++) {
-					
+					// For each possible value in each cell.
 					for (int h = 0; h < numBoxes; h++) {
-						int matrixIndex = this.getMatrixIndex(i + 1, j + 1, h + 1);
-						//System.out.println(matrixIndex);
+						int matrixIndex = this.getMatrixIndex(i, j, h);
 						this.binaryMatrix.get(matrixIndex).set(matrixStartIndex + boxStartingIndex + h, "1");
 					}
 				}
 			}
 			boxNum++;
-
 			// Adjust starting co-ordinates for next box.
 			if (coord2 == numBoxes - boxWidth) {
 				coord2 = 0;
@@ -170,18 +183,15 @@ public class AlgorXSolver extends StdSudokuSolver
 			} else {
 				coord2 += boxWidth;
 			}
-			
 			// Recur.
-			boxConstraints(coord1, coord2, boxWidth, boxNum);
+			boxConstraints(coord1, coord2, boxWidth, boxNum, matrixStartIndex);
 		}
 	}
-    
-    //Takes sudoku coords/value and returns that position in the matrix.
-    public int getMatrixIndex(int coord1, int coord2, int value) {
-    	
-    	return (coord1 - 1) * gridSize * gridSize 
-    		      + (coord2 - 1) * gridSize + (value - 1);	
-    }
-    
+	/*
+	 * Takes sudoku coords/value and returns that position in the matrix.
+	 */
+	public int getMatrixIndex(int coord1, int coord2, int value) {
+		return (coord1) * gridSize * gridSize + (coord2) * gridSize + (value);
+	}
 
 } // end of class AlgorXSolver
